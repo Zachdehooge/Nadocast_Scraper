84314a1c284c26e57e9bf643687395080a04c5b4
Zach <Zacharydehooge@gmail.com>
2024-09-09 11:16:04 -0400

    Update Nadocast

---
+++ Nadocast.sh
@@ -0,0 +1,1 @@
+python ./Nadocast_Automated.py
--- Nadocast_Automated.py
+++ Nadocast_Automated.py
@@ -28,18 +28,17 @@ elif 19 <= int(timeNow) <= 23:
 else:
     print("(-) No Nadocast Data To Fetch // Check If Statement Set For Constraints Covering Current Time: {0}".format(timeNow))
 
-#URL Structure 
+#URL Structure
 #url = "http://data.nadocast.com/202403/20240315/t0z/"
 
 url = "http://data.nadocast.com/{2}{1}/{2}{1}{0}/t{3}z/".format(day, month, year, timeNow)
-
 
 #If there is no such folder, the script will create one automatically
 folder_location = r'Nadocast\\{1}_{0}_{2}_{3}z'.format(day, month, year, timeNow)
 if not os.path.exists(folder_location):os.makedirs(folder_location)
 
 response = requests.get(url)
-soup= BeautifulSoup(response.text, "html.parser")     
+soup= BeautifulSoup(response.text, "html.parser")
 for link in soup.select("a[href$='.png']"):
     #Name the png files using the last portion of each link which are unique in this case
     filename = os.path.join(folder_location,link['href'].split('/')[-1])
