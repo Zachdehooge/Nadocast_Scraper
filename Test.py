ad295391a8b3a6d24e0aa7722f21fe4bb8a0cb99
Zach <Zacharydehooge@gmail.com>
2024-10-30 10:52:39 -0400

    Added IDNA Dependency

--- Test.py
+++ Test.py
@@ -1,6 +1,7 @@
 import unittest
 import requests
 
+# To run tests do: 
 class TestURL(unittest.TestCase):
     def test_list_int(self):
         """
@@ -9,5 +10,6 @@ class TestURL(unittest.TestCase):
         url = requests.get("http://data.nadocast.com")
         self.assertEqual(url.status_code, 200)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
